89c54def78d3e3b71d9302c6583b74556a21fc96
Stevdza-San <stefan.jovanavich@gmail.com>
2023-07-10 13:30:04 +0200

    usercheck API Endpoint created.

---
+++ site/src/jvmMain/kotlin/com/example/blogmultiplatform/api/UserCheck.kt
@@ -0,0 +1,49 @@
+package com.example.blogmultiplatform.api
+
+import com.example.blogmultiplatform.data.MongoDB
+import com.example.blogmultiplatform.models.User
+import com.example.blogmultiplatform.models.UserWithoutPassword
+import com.varabyte.kobweb.api.Api
+import com.varabyte.kobweb.api.ApiContext
+import com.varabyte.kobweb.api.data.getValue
+import com.varabyte.kobweb.api.http.setBodyText
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.encodeToString
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+
+@Api(routeOverride = "usercheck")
+suspend fun userCheck(context: ApiContext) {
+    try {
+        val userRequest =
+            context.req.body?.decodeToString()?.let { Json.decodeFromString<User>(it) }
+        val user = userRequest?.let {
+            context.data.getValue<MongoDB>().checkUserExistence(
+                User(username = it.username, password = hashPassword(it.password))
+            )
+        }
+        if (user != null) {
+            context.res.setBodyText(
+                Json.encodeToString(
+                    UserWithoutPassword(id = user.id, username = user.username)
+                )
+            )
+        } else {
+            context.res.setBodyText(Json.encodeToString(Exception("User doesn't exist.")))
+        }
+    } catch (e: Exception) {
+        context.res.setBodyText(Json.encodeToString(Exception(e.message)))
+    }
+}
+
+private fun hashPassword(password: String): String {
+    val messageDigest = MessageDigest.getInstance("SHA-256")
+    val hashBytes = messageDigest.digest(password.toByteArray(StandardCharsets.UTF_8))
+    val hexString = StringBuffer()
+
+    for (byte in hashBytes) {
+        hexString.append(String.format("%02x", byte))
+    }
+
+    return hexString.toString()
+}
--- site/src/jvmMain/kotlin/com/example/blogmultiplatform/models/User.kt
+++ site/src/jvmMain/kotlin/com/example/blogmultiplatform/models/User.kt
@@ -11,3 +11,10 @@ data class User(
     val username: String = "",
     val password: String = ""
 )
+
+@Serializable
+data class UserWithoutPassword(
+    @SerialName(value = "_id")
+    val id: String = ObjectIdGenerator.newObjectId<String>().id.toHexString(),
+    val username: String = ""
+)
