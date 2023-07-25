cbe56d14ffb91d401b22d9de78419bb55013be73
Stevdza-San <stefan.jovanavich@gmail.com>
2023-07-25 14:12:13 +0200

    MessagePopup implemented.

---
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/components/MessagePopup.kt
@@ -0,0 +1,65 @@
+package com.example.blogmultiplatform.components
+
+import androidx.compose.runtime.Composable
+import com.example.blogmultiplatform.models.Theme
+import com.example.blogmultiplatform.util.Constants.FONT_FAMILY
+import com.varabyte.kobweb.compose.css.TextAlign
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.ui.Alignment
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.modifiers.position
+import com.varabyte.kobweb.compose.ui.modifiers.textAlign
+import com.varabyte.kobweb.compose.ui.modifiers.width
+import com.varabyte.kobweb.compose.ui.modifiers.zIndex
+import com.varabyte.kobweb.silk.components.text.SpanText
+import org.jetbrains.compose.web.css.Position
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.vh
+import org.jetbrains.compose.web.css.vw
+
+@Composable
+fun MessagePopup(
+    message: String,
+    onDialogDismiss: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .width(100.vw)
+            .height(100.vh)
+            .position(Position.Fixed)
+            .zIndex(19),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .backgroundColor(Theme.HalfBlack.rgb)
+                .onClick { onDialogDismiss() }
+        )
+        Box(
+            modifier = Modifier
+                .padding(all = 24.px)
+                .backgroundColor(Colors.White)
+                .borderRadius(r = 4.px)
+        ) {
+            SpanText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .textAlign(TextAlign.Center)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(16.px),
+                text = message
+            )
+        }
+    }
+}
--- site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/admin/Create.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/admin/Create.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import com.example.blogmultiplatform.components.AdminPageLayout
+import com.example.blogmultiplatform.components.MessagePopup
 import com.example.blogmultiplatform.models.Category
 import com.example.blogmultiplatform.models.EditorKey
 import com.example.blogmultiplatform.models.Post
@@ -70,6 +71,7 @@ import com.varabyte.kobweb.silk.components.text.SpanText
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import kotlinx.browser.document
 import kotlinx.browser.localStorage
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.px
@@ -86,7 +88,7 @@ import org.w3c.dom.HTMLTextAreaElement
 import org.w3c.dom.get
 import kotlin.js.Date
 
-data class CreatePageUiEvent(
+data class CreatePageUiState(
     var id: String = "",
     var title: String = "",
     var subtitle: String = "",
@@ -97,7 +99,8 @@ data class CreatePageUiEvent(
     var popular: Boolean = false,
     var main: Boolean = false,
     var sponsored: Boolean = false,
-    var editorVisibility: Boolean = true
+    var editorVisibility: Boolean = true,
+    var messagePopup: Boolean = false
 )
 
 @Page
@@ -112,7 +115,7 @@ fun CreatePage() {
 fun CreateScreen() {
     val scope = rememberCoroutineScope()
     val breakpoint = rememberBreakpoint()
-    var uiEvent by remember { mutableStateOf(CreatePageUiEvent()) }
+    var uiState by remember { mutableStateOf(CreatePageUiState()) }
 
     AdminPageLayout {
         Box(
@@ -140,8 +143,8 @@ fun CreateScreen() {
                     ) {
                         Switch(
                             modifier = Modifier.margin(right = 8.px),
-                            checked = uiEvent.popular,
-                            onCheckedChange = { uiEvent = uiEvent.copy(popular = it) },
+                            checked = uiState.popular,
+                            onCheckedChange = { uiState = uiState.copy(popular = it) },
                             size = SwitchSize.LG
                         )
                         SpanText(
@@ -162,8 +165,8 @@ fun CreateScreen() {
                     ) {
                         Switch(
                             modifier = Modifier.margin(right = 8.px),
-                            checked = uiEvent.main,
-                            onCheckedChange = { uiEvent = uiEvent.copy(main = it) },
+                            checked = uiState.main,
+                            onCheckedChange = { uiState = uiState.copy(main = it) },
                             size = SwitchSize.LG
                         )
                         SpanText(
@@ -181,8 +184,8 @@ fun CreateScreen() {
                     ) {
                         Switch(
                             modifier = Modifier.margin(right = 8.px),
-                            checked = uiEvent.sponsored,
-                            onCheckedChange = { uiEvent = uiEvent.copy(sponsored = it) },
+                            checked = uiState.sponsored,
+                            onCheckedChange = { uiState = uiState.copy(sponsored = it) },
                             size = SwitchSize.LG
                         )
                         SpanText(
@@ -228,8 +231,8 @@ fun CreateScreen() {
                         }
                 )
                 CategoryDropdown(
-                    selectedCategory = uiEvent.category,
-                    onCategorySelect = { uiEvent = uiEvent.copy(category = it) }
+                    selectedCategory = uiState.category,
+                    onCategorySelect = { uiState = uiState.copy(category = it) }
                 )
                 Row(
                     modifier = Modifier.fillMaxWidth().margin(topBottom = 12.px),
@@ -238,8 +241,8 @@ fun CreateScreen() {
                 ) {
                     Switch(
                         modifier = Modifier.margin(right = 8.px),
-                        checked = !uiEvent.thumbnailInputDisabled,
-                        onCheckedChange = { uiEvent = uiEvent.copy(thumbnailInputDisabled = !it) },
+                        checked = !uiState.thumbnailInputDisabled,
+                        onCheckedChange = { uiState = uiState.copy(thumbnailInputDisabled = !it) },
                         size = SwitchSize.MD
                     )
                     SpanText(
@@ -251,55 +254,55 @@ fun CreateScreen() {
                     )
                 }
                 ThumbnailUploader(
-                    thumbnail = uiEvent.thumbnail,
-                    thumbnailInputDisabled = uiEvent.thumbnailInputDisabled,
+                    thumbnail = uiState.thumbnail,
+                    thumbnailInputDisabled = uiState.thumbnailInputDisabled,
                     onThumbnailSelect = { filename, file ->
                         (document.getElementById(Id.thumbnailInput) as HTMLInputElement).value =
                             filename
-                        uiEvent = uiEvent.copy(thumbnail = file)
+                        uiState = uiState.copy(thumbnail = file)
                     }
                 )
                 EditorControls(
                     breakpoint = breakpoint,
-                    editorVisibility = uiEvent.editorVisibility,
+                    editorVisibility = uiState.editorVisibility,
                     onEditorVisibilityChange = {
-                        uiEvent = uiEvent.copy(
-                            editorVisibility = !uiEvent.editorVisibility
+                        uiState = uiState.copy(
+                            editorVisibility = !uiState.editorVisibility
                         )
                     }
                 )
-                Editor(editorVisibility = uiEvent.editorVisibility)
+                Editor(editorVisibility = uiState.editorVisibility)
                 CreateButton(
                     onClick = {
-                        uiEvent =
-                            uiEvent.copy(title = (document.getElementById(Id.titleInput) as HTMLInputElement).value)
-                        uiEvent =
-                            uiEvent.copy(subtitle = (document.getElementById(Id.subtitleInput) as HTMLInputElement).value)
-                        uiEvent =
-                            uiEvent.copy(content = (document.getElementById(Id.editor) as HTMLTextAreaElement).value)
-                        if (!uiEvent.thumbnailInputDisabled) {
-                            uiEvent =
-                                uiEvent.copy(thumbnail = (document.getElementById(Id.thumbnailInput) as HTMLInputElement).value)
+                        uiState =
+                            uiState.copy(title = (document.getElementById(Id.titleInput) as HTMLInputElement).value)
+                        uiState =
+                            uiState.copy(subtitle = (document.getElementById(Id.subtitleInput) as HTMLInputElement).value)
+                        uiState =
+                            uiState.copy(content = (document.getElementById(Id.editor) as HTMLTextAreaElement).value)
+                        if (!uiState.thumbnailInputDisabled) {
+                            uiState =
+                                uiState.copy(thumbnail = (document.getElementById(Id.thumbnailInput) as HTMLInputElement).value)
                         }
                         if (
-                            uiEvent.title.isNotEmpty() &&
-                            uiEvent.subtitle.isNotEmpty() &&
-                            uiEvent.thumbnail.isNotEmpty() &&
-                            uiEvent.content.isNotEmpty()
+                            uiState.title.isNotEmpty() &&
+                            uiState.subtitle.isNotEmpty() &&
+                            uiState.thumbnail.isNotEmpty() &&
+                            uiState.content.isNotEmpty()
                         ) {
                             scope.launch {
                                 val result = addPost(
                                     Post(
                                         author = localStorage["username"].toString(),
-                                        title = uiEvent.title,
-                                        subtitle = uiEvent.subtitle,
+                                        title = uiState.title,
+                                        subtitle = uiState.subtitle,
                                         date = Date.now().toLong(),
-                                        thumbnail = uiEvent.thumbnail,
-                                        content = uiEvent.content,
-                                        category = uiEvent.category,
-                                        popular = uiEvent.popular,
-                                        main = uiEvent.main,
-                                        sponsored = uiEvent.sponsored
+                                        thumbnail = uiState.thumbnail,
+                                        content = uiState.content,
+                                        category = uiState.category,
+                                        popular = uiState.popular,
+                                        main = uiState.main,
+                                        sponsored = uiState.sponsored
                                     )
                                 )
                                 if (result) {
@@ -307,12 +310,22 @@ fun CreateScreen() {
                                 }
                             }
                         } else {
-                            println("Please fill out all fields.")
+                            scope.launch {
+                                uiState = uiState.copy(messagePopup = true)
+                                delay(2000)
+                                uiState = uiState.copy(messagePopup = false)
+                            }
                         }
                     }
                 )
             }
         }
+    }
+    if(uiState.messagePopup) {
+        MessagePopup(
+            message = "Please fill out all fields.",
+            onDialogDismiss = { uiState = uiState.copy(messagePopup = false)}
+        )
     }
 }
 
