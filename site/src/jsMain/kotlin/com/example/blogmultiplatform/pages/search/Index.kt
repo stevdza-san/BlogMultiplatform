a4567efe67fcae28db3d1bbb525c08422dfb7d6e
Stevdza-San <stefan.jovanavich@gmail.com>
2023-08-20 15:36:50 +0200

    Fixed an error with an empty category parameter on a Search Page. Added a loading indicator as well.

--- site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/search/Index.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/search/Index.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import com.example.blogmultiplatform.components.CategoryNavigationItems
+import com.example.blogmultiplatform.components.LoadingIndicator
 import com.example.blogmultiplatform.components.OverflowSidePanel
 import com.example.blogmultiplatform.models.ApiListResponse
 import com.example.blogmultiplatform.models.Category
@@ -49,6 +50,7 @@ fun SearchPage() {
     val context = rememberPageContext()
     val scope = rememberCoroutineScope()
 
+    var apiResponse by remember { mutableStateOf<ApiListResponse>(ApiListResponse.Idle) }
     var overflowOpened by remember { mutableStateOf(false) }
     val searchedPosts = remember { mutableStateListOf<PostWithoutDetails>() }
     var postsToSkip by remember { mutableStateOf(0) }
@@ -76,9 +78,11 @@ fun SearchPage() {
         postsToSkip = 0
         if (hasCategoryParam) {
             searchPostsByCategory(
-                category = Category.valueOf(value),
+                category = runCatching { Category.valueOf(value) }
+                    .getOrElse { Category.Programming },
                 skip = postsToSkip,
                 onSuccess = { response ->
+                    apiResponse = response
                     if (response is ApiListResponse.Success) {
                         searchedPosts.clear()
                         searchedPosts.addAll(response.data)
@@ -94,6 +98,7 @@ fun SearchPage() {
                 query = value,
                 skip = postsToSkip,
                 onSuccess = { response ->
+                    apiResponse = response
                     if (response is ApiListResponse.Success) {
                         searchedPosts.clear()
                         searchedPosts.addAll(response.data)
@@ -116,7 +121,9 @@ fun SearchPage() {
                 onMenuClose = { overflowOpened = false },
                 content = {
                     CategoryNavigationItems(
-                        selectedCategory = if (hasCategoryParam) Category.valueOf(value) else null,
+                        selectedCategory = if (hasCategoryParam) runCatching {
+                            Category.valueOf(value)
+                        }.getOrElse { Category.Programming } else null,
                         vertical = true
                     )
                 }
@@ -124,71 +131,78 @@ fun SearchPage() {
         }
         HeaderSection(
             breakpoint = breakpoint,
-            selectedCategory = if (hasCategoryParam) Category.valueOf(value) else null,
+            selectedCategory = if (hasCategoryParam) runCatching {
+                Category.valueOf(value)
+            }.getOrElse { Category.Programming } else null,
             logo = Res.Image.logo,
             onMenuOpen = { overflowOpened = true }
         )
-        if (hasCategoryParam) {
-            SpanText(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .textAlign(TextAlign.Center)
-                    .margin(top = 100.px, bottom = 40.px)
-                    .fontFamily(FONT_FAMILY)
-                    .fontSize(36.px),
-                text = value
-            )
-        }
-        PostsSection(
-            breakpoint = breakpoint,
-            posts = searchedPosts,
-            showMoreVisibility = showMorePosts,
-            onShowMore = {
-                scope.launch {
-                    if (hasCategoryParam) {
-                        searchPostsByCategory(
-                            category = Category.valueOf(value),
-                            skip = postsToSkip,
-                            onSuccess = { response ->
-                                if (response is ApiListResponse.Success) {
-                                    if (response.data.isNotEmpty()) {
-                                        if (response.data.size < POSTS_PER_PAGE) {
-                                            showMorePosts = false
-                                        }
-                                        searchedPosts.addAll(response.data)
-                                        postsToSkip += POSTS_PER_PAGE
-                                    } else {
-                                        showMorePosts = false
-                                    }
-                                }
-                            },
-                            onError = {}
-                        )
-                    } else if (hasQueryParam) {
-                        searchPostsByTitle(
-                            query = value,
-                            skip = postsToSkip,
-                            onSuccess = { response ->
-                                if (response is ApiListResponse.Success) {
-                                    if (response.data.isNotEmpty()) {
-                                        if (response.data.size < POSTS_PER_PAGE) {
-                                            showMorePosts = false
-                                        }
-                                        searchedPosts.addAll(response.data)
-                                        postsToSkip += POSTS_PER_PAGE
-                                    } else {
-                                        showMorePosts = false
-                                    }
-                                }
-                            },
-                            onError = {}
-                        )
-                    }
-                }
-            },
-            onClick = {
-
+        if (apiResponse is ApiListResponse.Success) {
+            if (hasCategoryParam) {
+                SpanText(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .textAlign(TextAlign.Center)
+                        .margin(top = 100.px, bottom = 40.px)
+                        .fontFamily(FONT_FAMILY)
+                        .fontSize(36.px),
+                    text = value.ifEmpty { Category.Programming.name }
+                )
             }
-        )
+            PostsSection(
+                breakpoint = breakpoint,
+                posts = searchedPosts,
+                showMoreVisibility = showMorePosts,
+                onShowMore = {
+                    scope.launch {
+                        if (hasCategoryParam) {
+                            searchPostsByCategory(
+                                category = runCatching { Category.valueOf(value) }
+                                    .getOrElse { Category.Programming },
+                                skip = postsToSkip,
+                                onSuccess = { response ->
+                                    if (response is ApiListResponse.Success) {
+                                        if (response.data.isNotEmpty()) {
+                                            if (response.data.size < POSTS_PER_PAGE) {
+                                                showMorePosts = false
+                                            }
+                                            searchedPosts.addAll(response.data)
+                                            postsToSkip += POSTS_PER_PAGE
+                                        } else {
+                                            showMorePosts = false
+                                        }
+                                    }
+                                },
+                                onError = {}
+                            )
+                        } else if (hasQueryParam) {
+                            searchPostsByTitle(
+                                query = value,
+                                skip = postsToSkip,
+                                onSuccess = { response ->
+                                    if (response is ApiListResponse.Success) {
+                                        if (response.data.isNotEmpty()) {
+                                            if (response.data.size < POSTS_PER_PAGE) {
+                                                showMorePosts = false
+                                            }
+                                            searchedPosts.addAll(response.data)
+                                            postsToSkip += POSTS_PER_PAGE
+                                        } else {
+                                            showMorePosts = false
+                                        }
+                                    }
+                                },
+                                onError = {}
+                            )
+                        }
+                    }
+                },
+                onClick = {
+
+                }
+            )
+        } else {
+            LoadingIndicator()
+        }
     }
 }
